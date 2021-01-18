cd5cdabc1479a99d3bef27b2a8850112cd75dfd0
Diego <diegomugnae@gmail.com>
2021-01-18 22:27:44 +1000

    Eslint

--- BestPractices/Ch01/01_04/begin/.eslintrc.js
+++ BestPractices/Ch01/01_04/begin/.eslintrc.js
@@ -1,8 +1,12 @@
+/* eslint-disable strict */
+
 module.exports = {
   env: {
     es6: true
   },
   rules: {
-
+    strict: ['error','global'],
+    'multiline-comment-style': ["error", "starred-block"],
+    'spaced-comment':['error','always']
   },
 };
---
+++ BestPractices/Ch01/01_04/begin/app.js
@@ -0,0 +1,14 @@
+/* 
+    Diego Mugnae
+    Curso de javsacript
+    ae maluco
+*/
+
+
+
+'use strict'
+
+// Variables
+const city = 'Chicago';
+console.log(city);
+
