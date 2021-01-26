b0480aceff23c972d942ad55925a8059d4499d0d
Diego <diegomugnae@gmail.com>
2021-01-26 21:30:34 +1000

    restaurant pt 1 - using filters

--- Code Challenges/01_03 Vegetarian Dinner/begin.js
+++ Code Challenges/01_03 Vegetarian Dinner/begin.js
@@ -1,3 +1,31 @@
+"use strict";
 /*
   WRITE YOUR SOLUTION HERE
 */
+
+const menu = [
+  {
+    name: "Chicken parmesan",
+    isVegetarian: false,
+  },
+  {
+    name: "Penne a la vodka",
+    isVegetarian: true,
+  },
+  {
+    name: "Mushroom risotto",
+    isVegetarian: true,
+  },
+  {
+    name: "Veal saltambuca",
+    isVegetarian: false,
+  },
+  {
+    name: "Filet mignon",
+    isVegetarian: false,
+  },
+];
+
+const vegetarianOptions = menu.filter(vegetarianDish => vegetarianDish.isVegetarian === true);
+
+console.log(vegetarianOptions);
