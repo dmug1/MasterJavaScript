cda2d4bc397c81afa12ebc25a9aed94a038bb8ed
Diego <diegomugnae@gmail.com>
2021-01-31 18:58:57 +1000

    coffee calculation

--- Code Challenges/01_09 Coffee Calculations/begin.js
+++ Code Challenges/01_09 Coffee Calculations/begin.js
@@ -1,3 +1,18 @@
 /*
   WRITE YOUR SOLUTION HERE
 */
+
+coffeeDate = (arr) => {
+    let coffeSum = arr.reduce((somaCafe , num) => 
+      somaCafe += num
+     );   
+
+    return `The total bill is $${coffeSum * 1.25}`;
+};
+
+coffeeDate([2, 5, 7, 1, 4]);
+
+
+
+
+console.log(coffeeDate([2, 5, 7, 1, 4]));
