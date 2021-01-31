a926f7f29f994a5b1f15aa5696fe05bff59d4a5f
Diego <diegomugnae@gmail.com>
2021-01-31 18:22:42 +1000

    change background clorur

--- Code Challenges/01_07 Classroom Attendance/begin.js
+++ Code Challenges/01_07 Classroom Attendance/begin.js
@@ -1,3 +1,22 @@
 /*
   WRITE YOUR SOLUTION HERE
 */
+
+const classroom =   {
+  hasTeachingAssistant: false,
+  classList: ["Rashida", "John", "Roman", "Lisa", "Omair", "Lukas"],
+};
+
+
+function getNames(classroom){
+
+  if (classroom.hasTeachingAssistant === false) {
+    return []
+  };
+};
+
+
+
+
+
+console.log(classroom);
--- Code Challenges/01_08 Dynamic Colors/begin.js
+++ Code Challenges/01_08 Dynamic Colors/begin.js
@@ -1,3 +1,18 @@
 /*
   WRITE YOUR SOLUTION HERE
 */
+
+document.getElementById("blue").addEventListener("click", function(){
+  mudaCor("blue");
+});
+document.getElementById("pink").addEventListener("click", function(){
+  mudaCor("pink");
+});
+document.getElementById("green").addEventListener("click", function(){
+  mudaCor("green");
+});
+
+
+function mudaCor(cor){
+  document.getElementsByTagName("body")[0].style.background  = cor;
+};
