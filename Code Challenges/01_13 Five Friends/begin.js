6ee8cbdb649e9d0ab68830c12cf32c522022dd41
Diego <diegomugnae@gmail.com>
2021-02-01 14:47:56 +1000

    async fetch

--- Code Challenges/01_13 Five Friends/begin.js
+++ Code Challenges/01_13 Five Friends/begin.js
@@ -3,3 +3,33 @@
 */
 
 //https://randomuser.me/api/?results=5
+
+
+// fetch('https://randomuser.me/api/?results=5')
+//   .then(
+//     response => response.json()
+//   )
+//   .then(data => {
+//     const dados = data.results;
+//     console.log('Sucess', data)
+//   })
+//   .catch(function () {
+//     console.log("Fail to fetch data")
+//   });
+
+
+async function fetchFriendsJSON(){
+  const response = await fetch('https://randomuser.me/api/?results=5');  
+  const friends = await response.json();
+  const timeline = document.querySelector("#timeline");  
+  console.log(friends);
+
+  friends.results.forEach((person) => {
+    let image = document.createElement("img");
+    image.src = person.picture.medium;
+    timeline.appendChild(image);
+  });
+}
+
+
+fetchFriendsJSON();
